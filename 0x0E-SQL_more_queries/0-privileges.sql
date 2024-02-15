689c99b729c6542b277e8dcff26d2da9130947a5
jessyovia <jessyovia100@gmail.com>
2024-02-14 18:24:28 -0800

    All

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,9 +1,4 @@
--- Lists all privileges of MySQL users user_0d_1 and user_0d_2 on localhost
-
--- For user_0d_1
-SELECT * FROM information_schema.user_privileges
-WHERE grantee LIKE 'user_0d_1@localhost';
-
--- For user_0d_2
-SELECT * FROM information_schema.user_privileges
-WHERE grantee LIKE 'user_0d_2@localhost';
+-- script that lists all privileges of the MySQL users
+-- user_0d_1 and user_0d_2 on your server (in localhost)
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
