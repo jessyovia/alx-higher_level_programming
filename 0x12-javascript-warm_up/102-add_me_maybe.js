8dc4a8d0fd01dcdf5a136d62ad4d3fc4fff863ee
jessyovia <jessyovia100@gmail.com>
2024-03-12 08:56:44 -0700

    102-add_me_maybe.js

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
+
+// Define the function to increment and call another function
 function addMeMaybe(number, theFunction) {
-  number++;
-  theFunction(number);
+  // Increment the number
+  const incrementedNumber = number + 1;
+  // Call the provided function with the incremented number
+  theFunction(incrementedNumber);
 }
+
+// Export the function to make it visible from outside
 module.exports.addMeMaybe = addMeMaybe;
