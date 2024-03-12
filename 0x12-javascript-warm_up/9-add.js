2f30602dce5664412b2136334e82ab5b25bb7031
jessyovia <jessyovia100@gmail.com>
2024-03-12 06:10:55 -0700

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 function add(a, b) {
-  return parseInt(a) + parseInt(b);
+  const numA = parseInt(a);
+  const numB = parseInt(b);
+  if (isNaN(numA) || isNaN(numB)) {
+    return NaN;
+  }
+  return numA + numB;
 }
 console.log(add(process.argv[2], process.argv[3]));
