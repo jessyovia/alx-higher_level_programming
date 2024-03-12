20b7a61ecb8480ad35dc6f9f7c711ec0e8090d0a
jessyovia <jessyovia100@gmail.com>
2024-03-12 08:33:59 -0700

    All

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+myObject.incr = function() {
+  this.value++;
+};
+
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
