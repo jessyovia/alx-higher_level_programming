616732551dfb7ceebfbb8f3d5267ea19f5ab6418
jessyovia <jessyovia100@gmail.com>
2024-03-12 16:45:43 -0700

    All'

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,15 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w <= 0 || h <= 0) {
-      // If width or height is not positive, create an empty object
       return {};
-    } else {
-      // Initialize instance attributes
-      this.width = w;
-      this.height = h;
     }
+    this.width = w;
+    this.height = h;
   }
 
-  print() {
-    // Print the rectangle using 'X'
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
