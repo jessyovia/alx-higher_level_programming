48bde599866cf70041f8c7f5c7b638f690723266
jessyovia <jessyovia100@gmail.com>
2024-03-12 09:37:51 -0700

    1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
+
 class Rectangle {
   constructor(w, h) {
-    if (w > 0 && h > 0) {
+    if (w <= 0 || h <= 0) {
+      // If width or height is not positive, return an empty object
+      return {};
+    } else {
+      // Initialize instance attributes
       this.width = w;
       this.height = h;
     }
