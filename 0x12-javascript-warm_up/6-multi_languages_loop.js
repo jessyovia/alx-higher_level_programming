054e0c6fe9dcaa602c1a516b3acc9d0c02bd7ead
jessyovia <jessyovia100@gmail.com>
2024-03-12 06:27:13 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for (const language of languages) {
-  console.log(language);
-}
+
+// Array of strings to print
+const txt = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Loop through each element of the array and print it
+for (let u = 0; u < txt.length; u++) { console.log(txt[u]); }
