009b05f42eb5c5f5973dc0973ffc6f40f17e6075
jessyovia <jessyovia100@gmail.com>
2024-03-22 18:42:54 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
+"""displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument"""
+
+
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-        cursor = db.cursor()
-        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-        cursor.execute(query, (state_name,))
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-        cursor.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3])
+
+    cur = conn.cursor()
+    state_name = sys.argv[4]
+    cur.execute(
+            'SELECT * FROM states WHERE CAST(name AS BINARY) LIKE ' +
+            'CAST("{}" AS BINARY) ORDER BY id ASC;'.format(state_name)
+        )
+    rows = cur.fetchall()
+
+    for res in rows:
+        print(res)
 
     cur.close()
     conn.close()
