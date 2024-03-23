d3f890c4e696b2858460c170179f036f19e62cbd
jessyovia <jessyovia100@gmail.com>
2024-03-22 18:45:16 -0700

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
+"""Lists all cities from the database hbtn_0e_4_usa"""
+
+
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-        cursor = db.cursor()
-        cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-        cursor.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3])
+
+    cur = conn.cursor()
+    query = ("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
+    cur.execute(query)
+    rows = cur.fetchall()
+
+    for res in rows:
+        print(res)
+
+    cur.close()
+    conn.close()
